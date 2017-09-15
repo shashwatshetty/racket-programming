c73de3c2b679234b2a38fd72f3c18fca952f8762
sha5hw4t <shashwat_shetty@hotmail.com>
2017-09-15 13:44:34 -0400

    Second Work Session

--- set01/q2.rkt
+++ set01/q2.rkt
@@ -3,3 +3,28 @@
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname q2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require rackunit)
 (require "extras.rkt")
+
+;; DATA DEFINITIONS:
+;; A FurlongLength is represented as a PosReal.
+;; A BarleycornLength is represented as a PosReal.
+
+
+;; CONTRACT & PURPOSE STATEMENT:
+;; furlons-to-barleycorns: FurlongLength -> BarleycornLength
+;; GIVEN: a length measured in furlongs,
+;; RETURNS: the equivalent length in barleycorns.
+
+;; EXAMPLES:
+;; (furlongs-to-bareycorns 10)= 237600
+;; (furlongs-to-barleycorns 1.5) = 35640
+
+;; TESTS:
+(begin-for-test
+  (check-equal? (furlongs-to-barleycorns 2) 47520
+     "(furlongs-to-barleycorns 2) should be equal to 47520")
+  (check-equal? (furlongs-to-barleycorns 0.1) 2376
+     "(furlongs-to-barleycorns 0.1) should be equal to 2376"))
+
+;; STRATEGY: Transcribe Formula for Furlongs to Barleycorns
+(define (furlongs-to-barleycorns f)
+  (* 23760 f))
--- set01/q3.rkt
+++ set01/q3.rkt
@@ -3,3 +3,34 @@
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname q3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require rackunit)
 (require "extras.rkt")
+
+
+;; DATA DEFINITIONS:
+;; A KelvinTemp is represented as a Real.
+;; A FahrenTemp is represented as a Real.
+
+
+;; CONTRACT & PURPOSE STATEMENTS:
+;; kelvin-to-fahrenheit: KelvinTemp -> FahrenTemp
+;; GIVEN: any temperature measured in kelvin,
+;; RETURNS: the equivalent temperature in fahrenheit.
+
+
+;; EXAMPLES:
+;; (kelvin-to-farhenheit 273.15) = 32
+;; (kelvin-to-farhenheit 0) = -459.67
+
+
+;; TESTS:
+(begin-for-test
+  (check-equal? (kelvin-to-farhenheit 300) 80.33
+       "(kelvin-to-farhenheit 300) should be equal to 80.33")
+  (check-equal? (kelvin-to-farhenheit -1) -461.47
+       "(kelvin-to-farhenheit -1) should be equal to -461.47")
+  (check-equal? (kelvin-to-farhenheit 0.5) -458.77
+       "(kelvin-to-farhenheit -1) should be equal to -461.47"))
+
+
+;; STRATEGY: Transcribe Formula for Kelvin to Farhenheit.
+(define (kelvin-to-farhenheit k)
+  (+ (* 9/5 k) (* -273.15 9/5) 32))
