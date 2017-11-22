2f6545fff88612f03087ee2e54edfedae126af27
shashwatshetty <shashwat_shetty@hotmail.com>
2017-11-21 22:13:59 -0500

    set10 1st work session

---
+++ set10/Player1.java
@@ -0,0 +1,76 @@
+// Constructor template for Player1:
+
+//     new Player1 (String name)
+//
+// Interpretation:
+//		name:  is the name of the player.
+
+public class Player1 implements Player{
+
+	private String playerName;     // name of the player.
+	private boolean underContract; // true iff player is under contract.
+	private boolean isInjured;	   // true iff player is injured.
+	private boolean isSuspended;   // true iff player is suspended.
+	
+	Player1(String name){
+		this.playerName = name;
+		this.underContract = false;
+		this.isInjured = false;
+		this.isSuspended = false;
+	}
+	
+	// Returns the name of this player.
+    
+	public String name() {
+		return this.playerName;
+	}
+
+	// Returns true iff this player is
+    //     under contract, and
+    //     not injured, and
+    //     not suspended
+	
+	public boolean available() {
+		return !(this.isInjured) && this.underContract && !(this.isSuspended);
+	}
+
+	// Returns true iff this player is under contract (employed).
+	
+	public boolean underContract() {
+		return this.underContract;
+	}
+
+	// Returns true iff this player is injured.
+	
+	public boolean isInjured() {
+		return this.isInjured;
+	}
+
+	// Returns true iff this player is suspended.
+
+	public boolean isSuspended() {
+		return this.isSuspended;
+	}
+
+	// Changes the underContract() status of this player
+    // to the specified boolean.
+	
+	public void changeContractStatus(boolean newStatus) {
+		this.underContract = newStatus;
+	}
+
+	// Changes the isInjured() status of this player
+    // to the specified boolean.
+	
+	public void changeInjuryStatus(boolean newStatus) {
+		this.isInjured = newStatus;
+	}
+
+	// Changes the isSuspended() status of this player
+    // to the specified boolean.
+	
+	public void changeSuspendedStatus(boolean newStatus) {
+		this.isSuspended = newStatus;
+	}
+
+}
